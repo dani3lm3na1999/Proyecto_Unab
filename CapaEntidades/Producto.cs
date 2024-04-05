d928cdcc88fd897a761dca7b52d20a181b055ca9
dani3lm3na1999 <dani3lm3na@gmail.com>
2024-04-04 18:24:27 -0600

    Agregar archivos de proyecto.

---
+++ CapaEntidades/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductoId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [MaxLength(250)]
+        public string Descripcion { get; set; }
+        [Required]
+        public decimal PrecioUnitario { get; set; }
+        [Required]
+        public int Existencias { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+
+    }
+}
