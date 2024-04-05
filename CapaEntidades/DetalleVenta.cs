037dab55cd4504f443be95c00c5e87537b7b70f0
dani3lm3na1999 <dani3lm3na@gmail.com>
2024-04-04 18:43:37 -0600

    DetalleVenta modificación

--- CapaEntidades/DetalleVenta.cs
+++ CapaEntidades/DetalleVenta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,22 @@ namespace CapaEntidades
 {
     public class DetalleVenta
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DetalleVentaId { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public decimal Precio { get; set; }
 
+        [Required]
+        public int ProductoId { get; set; }
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; }
+
+        [Required]
+        public int VentaId { get; set; }
+        [ForeignKey("VentaId")]
+        public Venta Venta { get; set; }
     }
 }
