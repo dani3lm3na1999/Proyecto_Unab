11069e280b9e8edad48e89b9be29bef1e4f67ecd
dani3lm3na1999 <dani3lm3na@gmail.com>
2024-04-04 18:34:44 -0600

    Creación de Entidad Venta

--- CapaEntidades/Venta.cs
+++ CapaEntidades/Venta.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,15 @@ namespace CapaEntidades
 {
     public class Venta
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VentaId { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public decimal Total { get; set; }
 
+        // propiedad de navegación
+        public List<DetalleVenta> Detalles { get; set; }
     }
 }
